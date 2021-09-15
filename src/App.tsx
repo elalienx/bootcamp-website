8b224c7ae8039bacebde0f1cc621ae0b3176fc8a
Eduardo <el_alienx@hotmail.com>
2021-09-15 18:15:47 +0200

    starting to add template CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+// Project files
+import "./styles/style.sass";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header className="hero container">
+        <h1 className="title">
+          Front
+          <br />
+          End
+          <br />
+          Course
+        </h1>
+        <h2 className="subtitle">
+          Training Program
+          <br />
+          Autum 2021
+        </h2>
+        <a href="#about">Learn more</a>
+      </header>
+      <section className="about">
+        <img src="" alt="" />
+        <div className="container">
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+            excepturi numquam aspernatur a ipsa qui tenetur vero iste dolorum,
+            consequuntur facere beatae omnis saepe, aliquid, consequatur quae.
+            Culpa, perspiciatis nesciunt!
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
