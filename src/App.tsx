1015bb3db6c23bd84851e5d8eff032afd4a486d8
Eduardo <el_alienx@hotmail.com>
2021-09-15 18:29:40 +0200

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
 // Project files
+import NavigationBar from "./components/NavigationBar";
 import "./styles/style.sass";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="hero container">
-        <h1 className="title">
-          Front
-          <br />
-          End
-          <br />
-          Course
-        </h1>
-        <h2 className="subtitle">
-          Training Program
-          <br />
-          Autum 2021
-        </h2>
-        <a href="#about">Learn more</a>
-      </header>
-      <section className="about">
-        <img src="" alt="" />
-        <div className="container">
-          <h2>About</h2>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
-            excepturi numquam aspernatur a ipsa qui tenetur vero iste dolorum,
-            consequuntur facere beatae omnis saepe, aliquid, consequatur quae.
-            Culpa, perspiciatis nesciunt!
-          </p>
-        </div>
-      </section>
+      <NavigationBar />
+      <div id="home">
+        {/* Hero */}
+        <header className="hero">
+          <div className="container">
+            <h1 className="title">
+              Front
+              <br />
+              End
+              <br />
+              Course
+            </h1>
+            <h2 className="subtitle">
+              Training Program
+              <br />
+              Autum 2021
+            </h2>
+            <a href="#about">Learn more</a>
+          </div>
+        </header>
+
+        {/* About */}
+        <section className="about">
+          <img src="" alt="" />
+          <div className="container">
+            <h2>About</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Provident excepturi numquam aspernatur a ipsa qui tenetur vero
+              iste dolorum, consequuntur facere beatae omnis saepe, aliquid,
+              consequatur quae. Culpa, perspiciatis nesciunt!
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,9 @@
+import Logo from "../assets/images/logo-white.png";
+
+export default function NavigationBar() {
+  return (
+    <nav id="navigation-bar">
+      <img src={Logo} alt="Novare logo with a handwritting overlay" />
+    </nav>
+  );
+}
