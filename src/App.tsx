78a02bd0a4b8d9e1f780d97e2a2de3f477b33933
Eduardo <el_alienx@hotmail.com>
2021-09-18 00:02:30 +0200

    added more css

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,14 @@
 // Project files
 import NavigationBar from "./components/NavigationBar";
 import "./styles/style.sass";
+// Project files
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <div id="home">
-        {/* Hero */}
-        <header className="hero">
-          <div className="container">
-            <h1 className="title">
-              Front
-              <br />
-              End
-              <br />
-              Course
-            </h1>
-            <h2 className="subtitle">
-              Training Program
-              <br />
-              Autum 2021
-            </h2>
-            <a href="#about">Learn more</a>
-          </div>
-        </header>
-
-        {/* About */}
-        <section className="about">
-          <img src="" alt="" />
-          <div className="container">
-            <h2>About the Academy</h2>
-            <p>
-              All participants have completed a 4 months Software Development
-              Academy. They learnt both basics and more advanced aspects of
-              object oriented programming with focus on Java.
-            </p>
-            <a className="button" href="#">
-              Learn more
-            </a>
-          </div>
-        </section>
-
-        {/* Candidates */}
-        <section className="candidates container">
-          <h2>Meet the candidates</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-          </p>
-          <ul>
-            <li>Armin Diszar</li>
-            <li>Clement Cardona</li>
-            <li>Dakouri Ikori</li>
-          </ul>
-          <button className="button" disabled>
-            View all candidates
-          </button>
-        </section>
-      </div>
+      <Home />
     </div>
   );
 }
