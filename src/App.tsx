a29e1e52afcea8411f7f2e4710833c5bf908a01e
Eduardo <el_alienx@hotmail.com>
2021-09-17 17:08:34 +0200

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,36 @@ export default function App() {
         </header>
 
         {/* About */}
-        <section id="about" className="about">
+        <section className="about">
           <img src="" alt="" />
           <div className="container">
-            <h2>About</h2>
+            <h2>About the Academy</h2>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident excepturi numquam aspernatur a ipsa qui tenetur vero
-              iste dolorum, consequuntur facere beatae omnis saepe, aliquid,
-              consequatur quae. Culpa, perspiciatis nesciunt!
+              All participants have completed a 4 months Software Development
+              Academy. They learnt both basics and more advanced aspects of
+              object oriented programming with focus on Java.
             </p>
+            <a className="button" href="#">
+              Learn more
+            </a>
           </div>
+        </section>
+
+        {/* Candidates */}
+        <section className="candidates container">
+          <h2>Meet the candidates</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <ul>
+            <li>Armin Diszar</li>
+            <li>Clement Cardona</li>
+            <li>Dakouri Ikori</li>
+          </ul>
+          <button className="button" disabled>
+            View all candidates
+          </button>
         </section>
       </div>
     </div>
