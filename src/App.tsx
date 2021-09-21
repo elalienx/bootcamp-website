c10808ebc379239bba0190327bdad06b5e247abd
Eduardo <el_alienx@hotmail.com>
2021-09-21 15:38:04 +0200

    added icons in about

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Project files
-import "./styles/style.sass";
 import ScrollToTop from "components/ScrollToTop";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
@@ -10,6 +9,8 @@ import About from "./pages/about";
 import Candidates from "./pages/candidates";
 import Home from "./pages/home";
 import Profile from "./pages/Profile";
+import "./scripts/font-awesome";
+import "./styles/style.sass";
 
 export default function App() {
   return (
--- src/components/ItemTechnology.tsx
+++ src/components/ItemTechnology.tsx
@@ -1,5 +1,7 @@
 // NPM Packages
 import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 
 // Project files
 import iTechnology from "interfaces/iTechnology";
@@ -11,11 +13,13 @@ interface MyProps {
 }
 
 const ItemTechnology: FC<MyProps> = ({ item }) => {
-  const { name, description } = item;
+  const { name, description, icon } = item;
 
   return (
     <li className="item-technology">
-      <div className="circle">@</div>
+      <div className="circle">
+        <FontAwesomeIcon icon={["fab", icon as IconName]} />
+      </div>
       <div className="content">
         <h3>{name}</h3>
         <p>{description}</p>
