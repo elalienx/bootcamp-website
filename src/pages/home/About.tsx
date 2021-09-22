70b5585d9af7edbea6ca3f19bd3fe5ccfef3fc0e
Eduardo <el_alienx@hotmail.com>
2021-09-22 23:20:46 +0200

    adding images from the desktop and thinking about the problem of layout

--- src/pages/home/About.tsx
+++ src/pages/home/About.tsx
@@ -3,24 +3,27 @@ import { Link } from "react-router-dom";
 
 // Project files
 import Image from "assets/images/about.jpg";
+import "./About.sass";
 
 export default function About() {
   return (
-    <section id="about" className="about">
-      <img
-        src={Image}
-        alt="A group of people sitting on a table using their laptops"
-      />
+    <section className="about">
       <div className="container">
-        <h2>About the Academy</h2>
-        <p>
-          All participants have completed a 4 months Software Development
-          Academy. They learnt both basics and more advanced aspects of object
-          oriented programming with focus on Java.
-        </p>
-        <Link className="button" to="/about">
-          Learn more
-        </Link>
+        <div className="content">
+          <h2>About the Academy</h2>
+          <p>
+            All participants have completed a 4 months Software Development
+            Academy. They learnt both basics and more advanced aspects of object
+            oriented programming with focus on Java.
+          </p>
+          <Link className="button" to="/about">
+            Learn more
+          </Link>
+        </div>
+        <img
+          src={Image}
+          alt="A group of people sitting on a table using their laptops"
+        />
       </div>
     </section>
   );
--- src/pages/home/Candidates.tsx
+++ src/pages/home/Candidates.tsx
@@ -6,6 +6,7 @@ import "./Candidates.sass";
 import data from "data/candidates.json";
 import iCandidate from "interfaces/iCandidate";
 import ItemCandidate from "components/ItemCandidate";
+import Image from "assets/images/candidates.jpg";
 
 export default function Candidates() {
   // Properties
@@ -32,16 +33,21 @@ export default function Candidates() {
   ));
 
   return (
-    <section className="candidates container">
-      <h2>Meet the candidates</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.{" "}
-      </p>
-      <ul id="foo">{CandidateItems}</ul>
-      <Link className="button" to="/candidates">
-        View all candidates
-      </Link>
+    <section className="candidates">
+      <div className="container">
+        <div className="content">
+          <h2>Meet the candidates</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <ul id="foo">{CandidateItems}</ul>
+          <Link className="button" to="/candidates">
+            View all candidates
+          </Link>
+        </div>
+        <img src={Image} alt="2 women sharing a single laptop" />
+      </div>
     </section>
   );
 }
