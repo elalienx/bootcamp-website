5b795a1378cf6bea21ff87fe9c3eb848d242e20f
Eduardo <el_alienx@hotmail.com>
2021-09-22 23:43:17 +0200

    fixing about

--- src/pages/about/Introduction.tsx
+++ src/pages/about/Introduction.tsx
@@ -1,20 +1,33 @@
 // Project files
 import Image from "assets/images/about.jpg";
+import "./Introduction.sass";
 
 export default function Introduction() {
   return (
     <header className="introduction">
       <img
+        className="mobile-hero"
         src={Image}
         alt="A group of people sitting on a table using their laptops"
       />
+
       <div className="container">
-        <h1>About the academy</h1>
-        <p>
-          All participants have completed a 4 months Software Development
-          Academy. They learnt both basics and more advanced aspects of object
-          oriented programming with focus on Java.
-        </p>
+        {/* Top - Left */}
+        <div className="content">
+          <h2>About the Academy</h2>
+          <p>
+            All participants have completed a 4 months Software Development
+            Academy. They learnt both basics and more advanced aspects of object
+            oriented programming with focus on Java.
+          </p>
+        </div>
+
+        {/* Bottom - Right */}
+        <img
+          className="support-picture"
+          src={Image}
+          alt="A group of people sitting on a table using their laptops"
+        />
       </div>
     </header>
   );
--- src/pages/about/MoreInformation.tsx
+++ src/pages/about/MoreInformation.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 // Project files
 import "./MoreInformation.sass";
+import Image from "assets/images/more-information.jpg";
 
 export default function MoreInformation() {
   // Properties
@@ -10,17 +11,33 @@ export default function MoreInformation() {
     "https://www.icloud.com/keynote/06byT0mJz_-qmghPbfRscHL8g#Marketing_presentation";
 
   return (
-    <section className="more-info container">
-      <h2>Want to know more?</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.{" "}
-      </p>
-      <a className="button" href={SLIDES_URL} target="_blank" rel="noreferrer">
-        Download full curriculum
-      </a>
-      <br />
-      <Link to="/candidates">Or meet the candidates</Link>
+    <section className="more-information">
+      <div className="container">
+        <div className="content">
+          <h2>Want to know more?</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+          </p>
+          <a
+            className="button"
+            href={SLIDES_URL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Download full curriculum
+          </a>
+          <br />
+          <Link className="link" to="/candidates">
+            Or meet the candidates
+          </Link>
+        </div>
+        <img
+          className="support-picture"
+          src={Image}
+          alt="2 women sharing a single laptop"
+        />
+      </div>
     </section>
   );
 }
