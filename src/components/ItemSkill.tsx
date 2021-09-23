a5622dce340fef5d7ec23b3e853806bdf279ee4f
Eduardo <el_alienx@hotmail.com>
2021-09-23 15:21:03 +0200

    polish

--- src/components/ItemSkill.tsx
+++ src/components/ItemSkill.tsx
@@ -15,7 +15,7 @@ const ItemSkill: FC<Props> = ({ item }) => {
 
   return (
     <li className="item-skill">
-      {name} {rating}
+      <span className="label">{name}</span>
       <div className="boxes">
         <div className={rating >= 1 ? "full" : ""}></div>
         <div className={rating >= 2 ? "full" : ""}></div>
