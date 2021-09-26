33b1cd89ae58ed810d375ca6a1c48d9849c0ee75
Eduardo <el_alienx@hotmail.com>
2021-09-26 13:53:10 +0200

    remove vanta until is further polished

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,35 +1,9 @@
-// NPM packages
-import { useState, useEffect, useRef } from "react";
-import * as THREE from "three"; // v124
-// @ts-ignore
-import GLOBE from "vanta/dist/vanta.globe.min"; // v5.31
-
 // Project files
 import "styles/pages/home/Hero.sass";
 
 export default function Hero() {
-  const [vantaEffect, setVantaEffect] = useState(0);
-  const myRef = useRef(null);
-
-  // Methods
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        GLOBE({
-          el: myRef.current,
-          THREE: THREE,
-          backgroundColor: 0x522b45,
-        })
-      );
-    }
-    return () => {
-      // @ts-ignore
-      if (vantaEffect) vantaEffect.destroy();
-    };
-  }, [vantaEffect]);
-
   return (
-    <header className="hero" ref={myRef}>
+    <header className="hero">
       <div className="container">
         <h1 className="title">
           Front
