840c80f5195337e85174c8a3a86b3c15d13e042f
Eduardo <el_alienx@hotmail.com>
2021-09-26 02:21:15 +0200

    adding vanta

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,9 +1,32 @@
+import { useState, useEffect, useRef } from "react";
+
+import * as THREE from "three";
+import GLOBE from "scripts/vanta.globe.min.js";
+
 // Project files
 import "styles/pages/home/Hero.sass";
 
 export default function Hero() {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        GLOBE({
+          el: myRef.current,
+          THREE: THREE,
+        })
+      );
+    }
+    return () => {
+      // @ts-ignore
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <header id="vanta-hero" className="hero">
+    <header id="vanta-hero" ref={myRef} className="hero">
       <div className="container">
         <h1 className="title">
           Front
