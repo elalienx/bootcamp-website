b7aa32568e7c5f2ed8c8deae5b51c78200b004bc
Eduardo <el_alienx@hotmail.com>
2021-11-05 09:24:41 +0100

    added jest dependencies

--- src/pages/home/randomCandidates.test.ts
+++ src/pages/home/randomCandidates.test.ts
@@ -1,6 +1,18 @@
-test("We are getting an array", () => {
-    
+// a quick dumb test to check if TypeScript works with Jest
+test("a quick demo test", () => {
+  // Arrange
+  const firstValue: number = 2;
+  const secondValue: number = 2;
+  const result: number = 4;
+
+  // Act
+  const addition: number = firstValue + secondValue;
+
+  // Assert
+  expect(addition).toBe(result);
 });
+
+// test("We are getting an array", () => {});
 
 // we are getting an array bigger than requested ammount of candidates
 
