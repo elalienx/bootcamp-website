d3e606325ea439566cfb15cb5548aa5ec7262d65
Eduardo <el_alienx@hotmail.com>
2021-11-05 09:19:44 +0100

    added type definitions for Jest unit testing in TypeScript

---
+++ src/pages/home/randomCandidates.test.ts
@@ -0,0 +1,7 @@
+test("We are getting an array", () => {
+    
+});
+
+// we are getting an array bigger than requested ammount of candidates
+
+// we need to test that the order of the array was randomized
