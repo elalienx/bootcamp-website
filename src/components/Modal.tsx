ea90a884bbb2a405885c120f03aa897ee88c0ef3
Eduardo <el_alienx@hotmail.com>
2021-11-26 14:29:12 +0100

    refactored modal into TS

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,12 @@ export default function Modal() {
   // Global state
   const { modal, setModal, PORTAL_ID } = useModal();
 
+  // Properties
+  const portalContainer = document.getElementById(PORTAL_ID);
+  const errorMessage = "Can't find the HTML tag for the modal";
+
   // Safeguards
+  if (portalContainer === null) throw new Error(errorMessage);
   if (modal === null) return null;
   if (modal === undefined) return null;
 
@@ -18,6 +23,6 @@ export default function Modal() {
       <div className="background" onClick={() => setModal(null)}></div>
       <div className="content">{modal}</div>
     </div>,
-    document.getElementById(PORTAL_ID)
+    portalContainer
   );
 }
