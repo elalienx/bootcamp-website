7d9728406bef1fdfea68434b4b952061ba82bafc
Eduardo <el_alienx@hotmail.com>
2022-01-12 11:38:28 +0100

    added the is hired to the profile page as well

--- src/pages/profile/Introduction.tsx
+++ src/pages/profile/Introduction.tsx
@@ -3,20 +3,25 @@ import PropsCandidate from "./PropsCandidate";
 import "styles/pages/profile/Introduction.sass";
 
 export default function Introduction({ candidate }: PropsCandidate) {
-  const { city, description, name, url, willing_to_relocate } = candidate;
+  const { isHired, city, description, name, url, willing_to_relocate } =
+    candidate;
 
   // Properties
   const imageObject = require(`assets/images/candidates/${url}.jpg`);
   const imageFile = imageObject.default;
   const relocationText = willing_to_relocate && " • Willing to relocate";
+  const showIsHired = isHired ? " • Already hired" : "";
 
   return (
     <header className="introduction container">
       <img className="avatar" src={imageFile} alt="Candidate thumbnail" />
-      <h1>{name}</h1>
+      <h1>
+        {name} {showIsHired}
+      </h1>
       <p className="subtitle">
         {city}
         {relocationText}
+        {showIsHired}
       </p>
       <p className="description">{description}</p>
     </header>
