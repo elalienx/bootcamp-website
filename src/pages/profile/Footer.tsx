d33335fcecbffa61330c2236f7501ab58e571076
Eduardo <el_alienx@hotmail.com>
2023-02-22 00:35:48 +0100

    fixed text remove cvs due to gdpr

--- src/pages/profile/Footer.tsx
+++ src/pages/profile/Footer.tsx
@@ -6,22 +6,17 @@ interface iProps {
 }
 
 export default function Footer({ item }: iProps) {
-  const { url, name, pronoun, portfolio_url } = item;
-
-  // Properties
-  const CV_URL = `http://novare-tech-education.web.app/files/student-cv/${url}.pdf`;
+  const { name, pronoun, portfolio_url } = item;
 
   return (
     <footer className="footer">
       <div className="container">
         <h2>Are you interested in {name}?</h2>
         <p>
-          See more details by checking {pronoun} CV and the projects that it has
-          done with us during the boot camp.
+          See more details by checking {pronoun} projects done with us during
+          the boot camp.
         </p>
-        <a target="_blank" className="button primary" href={CV_URL}>
-          Donwload CV
-        </a>
+
         <a target="_blank" className="button" href={portfolio_url}>
           View portfolio
         </a>
