570e8bb0dec85301274fc4bf9378cfde2657233e
Eduardo <el_alienx@hotmail.com>
2023-02-23 17:04:30 +0100

    text change

--- src/pages/profile/Footer.tsx
+++ src/pages/profile/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer({ item }: iProps) {
   return (
     <footer className="footer">
       <div className="container">
-        <h2>Are you interested in {name}?</h2>
+        <h2>Learn more about {name}?</h2>
         <p>
           See more details by checking {pronoun} projects done with us during
           the boot camp.
